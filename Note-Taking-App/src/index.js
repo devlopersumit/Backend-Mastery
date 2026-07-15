7e48bd8845282e2949bfabdc655ae7400e37995b
devlopersumit <sumitsj2002@gmail.com>
2026-07-15 17:25:23 +0530

    Refactor index.js to integrate notesRouter and remove welcome route

--- Note-Taking-App/src/index.js
+++ Note-Taking-App/src/index.js
@@ -1,18 +1,18 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const notesRouter = require('./routes/notes.routes');
 require('dotenv').config();
 
 const app  = express();
 
+//Built-in Middlewares
 app.use(express.json());
 
+//DB CONNECTION
 connectDB();
 
-app.get("/", (req, res) => {
-    res.json({
-        message: "Welcome To Note Taking App Server"
-    });
-});
+// ROUTES
+app.use("/api/notes", notesRouter);
 
 const port = process.env.PORT | 6000;
 app.listen(port, () => {
--- Note-Taking-App/src/routes/notes.routes.js
+++ Note-Taking-App/src/routes/notes.routes.js
@@ -1,5 +1,16 @@
 const express = require("express");
+const { getAllNotes, createNote, updateNoteById, deleteNoteById, getNoteById } = require("../controllers/notes.controllers");
 
 const notesRouter = express.Router();
 
 notesRouter.route("/")
+           .get(getAllNotes)
+           .post(createNote);
+
+ notesRouter.route("/:id")
+            .put(updateNoteById)
+            .delete(deleteNoteById)
+            .get(getNoteById);
+
+
+module.exports = notesRouter;
