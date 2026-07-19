f606ceb566f7d47fe2ac47cce673a43f8f5438e9
devlopersumit <sumitsj2002@gmail.com>
2026-07-19 16:32:42 +0530

    setup userSchema with automatic password hashing

--- Multi-Tenant-Notes-App/src/config/db.js
+++ Multi-Tenant-Notes-App/src/config/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 export const connectMongoDB = async () => {
     try {
         await mongoose.connect(process.env.MONGODB_URI);
-        console.log(`MngoDB Conneceted Successfully✅`);
+        console.log(`MongoDB Conneceted Successfully✅`);
     } catch (error) {
         console.log(`MongoDB Connection Error: ${error.message}`);
         process.exit(1);
---
+++ Multi-Tenant-Notes-App/src/models/user.models.js
@@ -0,0 +1,63 @@
+import mongoose from 'mongoose';
+import bcrypt from 'bcryptjs';
+
+const userSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: [true, "firstname is required"],
+        trim: true,
+        minLength: [3, 'firstname length must be atleast 3 character']
+    },
+    lastName: {
+        type: String,
+        trim: true
+    },
+    email: {
+        type: String,
+        required: [true, 'Email address is required'],
+        unique: true,
+        trim: true,
+        lowercase: true,
+        validate: {
+            validator: function (v) {
+                return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(v);
+            },
+            message: props => `${props.value} is not a valid email address!`
+        }
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+        // Use select: false so the hash isn't accidentally leaked in API responses
+        select: false,
+        validate: {
+            validator: function (v) {
+                // Enforces: Min 8 chars, 1 uppercase, 1 lowercase, 1 number, 1 special char
+                return /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/.test(v);
+            },
+            message: () =>
+                'Password must be at least 8 characters long and contain at least ' +
+                'one uppercase letter, one lowercase letter, one number, and one special character.'
+        }
+    }
+}, {
+    timestamps: true
+});
+
+// Pre-save middleware to automatically hash the password
+userSchema.pre('save', async function (next) {
+    if (!this.isModified('password')) return next();
+    try {
+        this.password = await bcrypt.hash(this.password, 10);
+        next();
+    } catch (err) {
+        next(err);
+    }
+});
+
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+const User = mongoose.model("User", userSchema);
+export default User;
